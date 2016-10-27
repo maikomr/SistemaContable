2347935c0df3c23d7a11ca2cafe10a4a4102c767
Maiko Morales <maiko.moralesr@gmail.com>
2016-10-27 04:28:16 -0400

    Added header and footer templates

---
+++ application/views/templates/footer.php
@@ -0,0 +1,10 @@
+<footer>
+        <p>&copy; 2016 Softline - UMSS.</p>
+    </footer>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="../../js/jquery-3.1.1.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="../../js/bootstrap.min.js"></script>
+</body>
+</html>
