64cf8a06bd25338fa1e0671d9a3ae33a78353054
Maiko Morales <maiko.moralesr@gmail.com>
2016-10-27 04:28:44 -0400

    Use templates in home page

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,6 +20,8 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
+		$this->load->view('templates/header');
 		$this->load->view('pages/welcome');
+		$this->load->view('templates/footer');
 	}
 }
