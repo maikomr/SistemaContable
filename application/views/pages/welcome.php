4bf55d3db87fcbe951b7aafbfacdd0541e28012c
Maiko Morales <maiko.moralesr@gmail.com>
2016-10-27 04:21:56 -0400

    Add home page [#133220995]

---
+++ application/views/pages/welcome.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Sistema contable</title>
+        <link rel="stylesheet" href="../../css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="../../css/main.css">
+    </head>
+<body>
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Sistema Contable</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="http://sistema-contable">Inicio</a></li>
+            <li><a href="http://sistema-contable/index.php/libro-diario">Libro Diario</a></li>
+          </ul>
+          <form class="navbar-form navbar-right">
+            <div class="form-group">
+              <input type="text" placeholder="Usuario" class="form-control">
+            </div>
+            <div class="form-group">
+              <input type="password" placeholder="Contraseña" class="form-control">
+            </div>
+            <button type="submit" class="btn btn-success">Ingresar</button>
+          </form>
+        </div>
+      </div>
+    </nav>
+    <div class="container">
+        <div class="logo-content">
+            <img src="../../img/lentesKawai.png" >
+            <div class="title m-b-md">
+                <h1>Bienvenido a Softline Sistema Contable</h1>
+            </div>
+        </div>
+    </div>
+    <footer>
+        <p>&copy; 2016 Company, Inc.</p>
+    </footer>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="../../js/jquery-3.1.1.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="../../js/bootstrap.min.js"></script>
+</body>
+</html>
