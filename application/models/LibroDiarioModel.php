b7543f4dd27a356ccf0dc186601df192ceffab8f
Maiko Morales <maiko.moralesr@gmail.com>
2016-12-02 17:26:14 -0400

    Create table accounts and update view in libro-diario

--- application/models/LibroDiarioModel.php
+++ application/models/LibroDiarioModel.php
@@ -7,18 +7,31 @@ class LibroDiarioModel extends CI_Model
 	}
 
 	public function registerTransaction(){
-		$data= array(
-			'account'=>$this->input->post('cuenta'),
+		$account = array('name'=>$this->input->post('cuenta'));
+		$this->db->trans_start();
+		$this->db->insert('accounts', $account);
+		$accountId = $this->db->insert_id();
+		$transaction = array(
 			'date'=>$this->input->post('fecha'),
 			'type'=>$this->input->post('tipoTransaccion'),
 			'payrate'=>$this->input->post('asientoContable'),
+			'accountId'=>$accountId,
 		);
-		return $this->db->insert('transaction', $data);
+		$result = $this->db->insert('transaction', $transaction);
+		$this->db->trans_complete();
+		return $result;
 	}
 	
 	public function getAllTransactions(){
-		$query = $this->db->get('transaction');
+		$query = $this->db->query('SELECT A.name as account, T.idTransaction, T.date, T.type, T.payrate FROM accounts A, transaction T WHERE A.id = T.accountId');
 		return $query->result();
+	}
+
+	public function getAccount($id) {
+		$this->db->select('*');
+		$this->db->from('accounts');
+		$this->db->where('id', $id);
+		return $this->db->get();
 	}
 }
 
--- contaumssg2.sql
+++ contaumssg2.sql
@@ -1,84 +1,47 @@
--- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 26-10-2016 a las 22:48:56
--- Versión del servidor: 10.1.16-MariaDB
--- Versión de PHP: 5.6.24
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+-- -----------------------------------------------------
+-- Schema contaumssg2
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `contaumssg2` DEFAULT CHARACTER SET latin1 ;
+USE `contaumssg2` ;
+
+-- -----------------------------------------------------
+-- Table `contaumssg2`.`accounts`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `contaumssg2`.`accounts` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(50) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `name_UNIQUE` (`name` ASC))
+ENGINE = MyISAM
+DEFAULT CHARACTER SET = latin1;
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+-- -----------------------------------------------------
+-- Table `contaumssg2`.`transaction`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `contaumssg2`.`transaction` (
+  `idTransaction` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `date` DATE NOT NULL,
+  `type` TEXT NOT NULL,
+  `payrate` INT(11) NOT NULL,
+  `accountId` INT(11) NOT NULL,
+  PRIMARY KEY (`idTransaction`),
+  UNIQUE INDEX `idTransaction` (`idTransaction` ASC),
+  INDEX `fk_transaction_accounts_idx` (`accountId` ASC),
+  CONSTRAINT `fk_transaction_accounts`
+    FOREIGN KEY (`accountId`)
+    REFERENCES `contaumssg2`.`accounts` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB
+AUTO_INCREMENT = 26
+DEFAULT CHARACTER SET = latin1;
 
---
--- Base de datos: `contaumssg2`
---
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `transaction`
---
-
-CREATE TABLE `transaction` (
-  `idTransaction` bigint(20) UNSIGNED NOT NULL,
-  `account` text NOT NULL,
-  `date` date NOT NULL,
-  `type` text NOT NULL,
-  `payrate` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Volcado de datos para la tabla `transaction`
---
-
-INSERT INTO `transaction` (`idTransaction`, `account`, `date`, `type`, `payrate`) VALUES
-(1, 'pruebaa', '2016-10-05', '', 200),
-(2, 'prueba 2', '2016-10-08', '', 12134),
-(3, 'prueba 3', '2016-10-08', '', 12345),
-(4, 'asdasd', '2016-10-12', '', 45678),
-(5, 'dfgdfhgh', '2016-10-15', '', 98746513),
-(6, 'mhgfj', '2016-10-08', '', 1580),
-(7, 'documentos por pagar', '2016-10-26', '', 51654),
-(8, 'jgfdgfh', '2016-10-26', '', 54675),
-(9, 'otra', '2016-10-26', '', 54675),
-(10, 'otra mas', '2016-10-26', '', 54675),
-(11, 'otrita mas', '2016-10-26', '', 54675),
-(12, 'jhlkjhk', '2016-10-08', '', 520),
-(13, 'jhlkjhk', '2016-10-08', '', 520),
-(14, 'jhlkjhk', '2016-10-08', '', 520),
-(15, 'kjgkjhgh', '2016-10-15', '', 764376),
-(16, 'mvnvn', '2016-10-08', '', 5555555),
-(17, 'mvnvn', '2016-10-08', '', 5555555),
-(18, 'mvnvn', '2016-10-08', '', 5555555),
-(19, 'asdasd', '2016-10-26', 'DEBE', 7894);
-
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `transaction`
---
-ALTER TABLE `transaction`
-  ADD PRIMARY KEY (`idTransaction`),
-  ADD UNIQUE KEY `idTransaction` (`idTransaction`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `transaction`
---
-ALTER TABLE `transaction`
-  MODIFY `idTransaction` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
