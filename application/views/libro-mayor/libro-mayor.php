2ec1085e49e01a81355a798df7798a47def1dc5d
Maiko Morales <maiko.moralesr@gmail.com>
2016-12-02 21:22:26 -0400

    Formatting libro mayor

--- application/views/libro-mayor/libro-mayor.php
+++ application/views/libro-mayor/libro-mayor.php
@@ -1,6 +1,43 @@
 <div class="container content">
     <div class="navbar-header">
       <h1>Libro mayor</h1>
-      <h3>Aqui deberia estar el libro mayor de la cuenta <?php echo $id; ?></h3>
+      <h3>Cuenta: <?php echo $accountName; ?></h3>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 form">
+                <table class="table table-bordered table-hover">
+                    <tr class= "primary-color-background">
+                        <td><strong>Debe</strong></td>
+                        <td><strong>Haber</strong></td>
+                    </tr>
+                    <?php
+                        $counter = 0;
+                        while ($counter < count($debit) || $counter < count($credit)) {
+                    ?>
+                        <tr>
+                        <td>
+                    <?php
+                        if ($debit != false && $counter < count($debit)) {
+                            echo $debit[$counter]->payrate;
+                        } else {
+                            echo '';
+                        }
+                    ?>
+                        </td>
+                        <td>
+                    <?php
+                        if ($credit != false && $counter < count($credit)) {
+                            echo $credit[$counter]->payrate;
+                        } else {
+                            echo '';
+                        }
+                    ?>
+                        </td>
+                    </tr>
+                    <?php $counter = $counter + 1; } ?>
+                </table>
+            </div>
+        </div>
     </div>
 </div>
