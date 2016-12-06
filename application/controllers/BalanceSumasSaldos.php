c04ba4dd90433996f74b02d432ddf85197114b6d
Nicole Maldonado <melomc02@gmail.com>
2016-12-06 00:42:58 -0400

    Implemented queries for retrieving sumas and saldos [#133186737]

--- application/controllers/BalanceSumasSaldos.php
+++ application/controllers/BalanceSumasSaldos.php
@@ -9,6 +9,50 @@ class BalanceSumasSaldos extends CI_CONTROLLER
 
     public function index()
     {
+        $accounts = $this->BalanceSumasSaldosModel->getAllAccounts();
+        $balances = array();
+
+        foreach ($accounts as $account) {
+            $debit = $this->BalanceSumasSaldosModel->getDebit($account->id);
+            $credit = $this->BalanceSumasSaldosModel->getCredit($account->id);
+            
+            $totalDebit = 0;
+            $totalCredit = 0;
+            $saldoDebit = 0;
+            $saldoCredit = 0;
+
+            if ($debit != false) {
+                foreach ($debit as $transaction) {
+                    $totalDebit += $transaction->payrate;
+                }
+            }
+            
+            if ($credit != false) {
+                foreach ($credit as $transaction) {
+                    $totalCredit += $transaction->payrate;
+                }
+            }
+            
+            if ($totalDebit > $totalCredit) {
+                $saldoDebit = $totalDebit - $totalCredit;
+            } else if ($totalCredit > $totalDebit) {
+                $saldoCredit = $totalCredit - $totalDebit;
+            }
+
+            $balance = new stdClass();
+            $balance->name = $account->name;
+            $balance->totalDebit = $totalDebit;
+            $balance->totalCredit = $totalCredit;
+            $balance->saldoDebit = $saldoDebit;
+            $balance->saldoCredit = $saldoCredit;
+            $balances[] = $balance;
+        }
+        
+        
+
+        $data['balances'] = $balances;
+
+
         $this->load->view('templates/header');
         $this->load->view('balance-sumas-saldos/index');
         $this->load->view('templates/footer');
--- application/models/BalanceSumasSaldosModel.php
+++ application/models/BalanceSumasSaldosModel.php
@@ -5,5 +5,42 @@ class BalanceSumasSaldosModel extends CI_Model
         parent::__construct();
         $this->load->database();
     }
+
+    public function getAllAccounts(){
+        $this->db->order_by('name');
+        $query = $this->db->get('accounts');
+        
+        if ($query->num_rows() > 0) {
+            return $query->result();
+        } else {
+            return false;
+        }
+    }
+
+    public function getDebit($id) {
+        $this->db->select('payrate');
+        $this->db->from('transaction');
+        $this->db->where('accountId',$id);
+        $this->db->where('type','DEBE');
+        $query = $this->db->get();
+        if ($query->num_rows() > 0) {
+            return $query->result();
+        } else {
+            return false;
+        }
+    }
+
+    public function getCredit($id) {
+        $this->db->select('payrate');
+        $this->db->from('transaction');
+        $this->db->where('accountId',$id);
+        $this->db->where('type','HABER');
+        $query = $this->db->get();
+        if ($query->num_rows() > 0) {
+            return $query->result();
+        } else {
+            return false;
+        }
+    }
 }
 ?>
