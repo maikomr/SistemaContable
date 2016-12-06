632130ff061a9112ccad0b98625030c9893f2d17
Maiko Morales <maiko.moralesr@gmail.com>
2016-12-05 23:39:24 -0400

    Update data base script in order to drop tables and schema when creating them

--- contaumssg2.sql
+++ contaumssg2.sql
@@ -5,24 +5,25 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 -- -----------------------------------------------------
 -- Schema contaumssg2
 -- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `contaumssg2` ;
 CREATE SCHEMA IF NOT EXISTS `contaumssg2` DEFAULT CHARACTER SET latin1 ;
 USE `contaumssg2` ;
 
 -- -----------------------------------------------------
 -- Table `contaumssg2`.`accounts`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `contaumssg2`.`accounts` ;
 CREATE TABLE IF NOT EXISTS `contaumssg2`.`accounts` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(50) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `name_UNIQUE` (`name` ASC))
-ENGINE = MyISAM
-DEFAULT CHARACTER SET = latin1;
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `contaumssg2`.`transaction`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `contaumssg2`.`transaction` ;
 CREATE TABLE IF NOT EXISTS `contaumssg2`.`transaction` (
   `idTransaction` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
   `date` DATE NOT NULL,
@@ -30,16 +31,13 @@ CREATE TABLE IF NOT EXISTS `contaumssg2`.`transaction` (
   `payrate` INT(11) NOT NULL,
   `accountId` INT(11) NOT NULL,
   PRIMARY KEY (`idTransaction`),
-  UNIQUE INDEX `idTransaction` (`idTransaction` ASC),
   INDEX `fk_transaction_accounts_idx` (`accountId` ASC),
   CONSTRAINT `fk_transaction_accounts`
     FOREIGN KEY (`accountId`)
     REFERENCES `contaumssg2`.`accounts` (`id`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE)
-ENGINE = InnoDB
-AUTO_INCREMENT = 26
-DEFAULT CHARACTER SET = latin1;
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
